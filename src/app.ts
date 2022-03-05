18ff16730a7f564945a0704be015eb9d826600be
Jesus <js.valiente92@gmail.com>
2022-03-05 12:39:39 +0100

    Initial setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+
+import { droidRoutes } from "./api/routes";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 8888);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 })
+);
+
+droidRoutes(app);
+
+export default app;
